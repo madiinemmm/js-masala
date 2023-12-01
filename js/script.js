d973206787567dcd1489e788569621bf71de0131
madiinemm <uraimovamadina179@gmail.com>
2023-12-01 21:28:16 +0500

    masala ishlandi

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// let a = 9732738.6;
+// let b = 1843789.6;
+// let res;
+// if (a > b ) {
+//     res = "Alisher ozigna sabr qlishi kerak"
+// } else {
+//     res = "Alisher oq yo'l"
+// } console.log(res);
